61a6592f25ca3b3878f766d9478d4e83a908b00a
Aman Adams <ST10290748@vcconnect.edu.za>
2024-08-29 19:16:05 +0200

    Add project files.

---
+++ CLDV2POE/Services/BlobService.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CLDV2POE.Services
+{
+    public class BlobService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public BlobService(IConfiguration configuration)
+        {
+            _blobServiceClient = new BlobServiceClient(configuration["AzureStorage:ConnectionString"]);
+        }
+
+        public async Task UploadBlobAsync(string containerName, string blobName, Stream content)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            await containerClient.CreateIfNotExistsAsync();
+            var blobClient = containerClient.GetBlobClient(blobName);
+            await blobClient.UploadAsync(content, true);
+        }
+    }
+}
---
+++ CLDV2POE/Services/QueueService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Azure.Storage.Queues;
+using System.Threading.Tasks;
+
+namespace CLDV2POE.Services
+{
+    public class QueueService
+    {
+        private readonly QueueServiceClient _queueServiceClient;
+
+        public QueueService(IConfiguration configuration)
+        {
+            _queueServiceClient = new QueueServiceClient(configuration["AzureStorage:ConnectionString"]);
+        }
+
+        public async Task SendMessageAsync(string queueName, string message)
+        {
+            var queueClient = _queueServiceClient.GetQueueClient(queueName);
+            await queueClient.CreateIfNotExistsAsync();
+            await queueClient.SendMessageAsync(message);
+        }
+    }
+}
